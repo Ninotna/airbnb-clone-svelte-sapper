9f5b08bbc270108601a0cdb2c1903abde9f7922c
Flavio Copes <copesc@gmail.com>
2019-10-16 22:00:20 +0200

    module-4-lesson-4

--- src/components/RegistrationModal.svelte
+++ src/components/RegistrationModal.svelte
@@ -1,16 +1,25 @@
 <script>
-	import { createEventDispatcher } from 'svelte'
-	const dispatch = createEventDispatcher()
+  import { createEventDispatcher } from 'svelte'
+  import axios from 'axios'
+
+  const dispatch = createEventDispatcher()
+
+	let email = ''
+	let password = ''
+	let passwordconfirmation = ''
+
+	const submit = async () => {
+    const response = await axios.post('auth/register', { email, password, passwordconfirmation })
+    console.log(response)
+	}
 </script>
 
 <h2>Sign up</h2>
 <div>
-  <form on:submit|preventDefault={() => {
-    alert('sign up!')
-  }}>
-    <input id="email" type="email" placeholder="Email address" />
-    <input id="password" type="password" placeholder="Password" />
-    <input id="passwordconfirmation" type="password" placeholder="Enter password again" />
+  <form on:submit|preventDefault={submit}>
+    <input bind:value={email} type="email" placeholder="Email address" />
+    <input bind:value={password} type="password" placeholder="Password" />
+    <input bind:value={passwordconfirmation} type="password" placeholder="Enter password again" />
     <button class="styled">Sign up</button>
     <p>Already have an account? <a href="javascript:;" on:click={() => dispatch('showLogin')}>Log in</a></p>
   </form>

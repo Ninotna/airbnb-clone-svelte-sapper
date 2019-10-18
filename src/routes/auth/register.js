a327bb197ba9cbcf0caf7e7ec8f2c44ca3383687
Flavio Copes <copesc@gmail.com>
2019-10-18 22:00:30 +0200

    module-4-lesson-6

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,4 +1,26 @@
 export const post = (req, res) => {
 	res.end('POST request received correctly')
 	console.log(req.body)
+import { User } from '../../model.js'
+export const post = async (req, res) => {
+	const { email, password, passwordconfirmation } = req.body
+
+	if (password !== passwordconfirmation) {
+		res.statusCode = 500
+		res.end(JSON.stringify({ status: 'error', message: 'Passwords do not match' }))
+		return
+	}
+
+	try {
+		const user = await User.create({ email, password })
+			return res.end(JSON.stringify({ status: 'success', message: 'Logged in' }))
+	} catch (error) {
+		let message = 'An error occurred'
+    if (error.name === 'SequelizeUniqueConstraintError') {
+			message = 'User already exists'
+		}
+
+		res.statusCode = 500
+		res.end(JSON.stringify({ status: 'error', message }))
+	}
 }

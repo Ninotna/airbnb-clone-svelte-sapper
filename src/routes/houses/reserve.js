b7c61dc3d7ed78282250347f44602b8bd8d9dcc4
Flavio Copes <copesc@gmail.com>
2019-10-21 17:19:00 +0200

    Server-side check to see if house is not booked, and if I'm logged in, too

--- src/routes/houses/reserve.js
+++ src/routes/houses/reserve.js
@@ -1,16 +1,57 @@
+import { Op } from 'sequelize'
+
 import Booking from '../../models/booking.js'
 import User from '../../models/user.js'
 
 export const post = async (req, res, next) => {
+  const startDate = req.body.startDate
+  const endDate = req.body.endDate
+  const houseId = req.body.houseId
 
+  const results = await Booking.findAll({
+    where: {
+      houseId: houseId,
+      startDate: {
+        [Op.lte]: new Date(endDate)
+      },
+      endDate: {
+        [Op.gte]: new Date(startDate)
+      }
+    }
+  })
+
+  if (results.length > 0) {
+    //busy
+    res.writeHead(500, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      status: 'error',
+      message: 'House is already booked'
+    }))
+
+    return
+  }
+
+  if (!req.session.passport) {
+    res.writeHead(403, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      status: 'error',
+      message: 'Unauthorized'
+    }))
+
+    return
+  }
   const userEmail = req.session.passport.user
   User.findOne({ where: {email: userEmail}}).then(user => {
 
     Booking.create({
-      houseId: req.body.houseId,
+      houseId: houseId,
       userId: user.id,
-      startDate: req.body.startDate,
-      endDate: req.body.endDate
+      startDate: startDate,
+      endDate: endDate
     })
     .then(() => {
       res.writeHead(200, {

76a5a2cc2f1151e92ec1bbe85526cf4a939eb41b
Flavio Copes <copesc@gmail.com>
2019-10-24 14:19:55 +0200

    Handle session

--- src/routes/bookings/index.svelte
+++ src/routes/bookings/index.svelte
@@ -1,18 +1,19 @@
-<script context="module">
-	export async function preload({ params, query }) {
-		const res = await this.fetch(`bookings/list.json`)
-		const data = await res.json()
-
-		if (res.status === 200) {
-			return { bookings: data }
-		} else {
-			this.error(res.status, data.message);
-		}
-	}
-</script>
-
 <script>
+import { onMount } from 'svelte'
+
+let loading = true
 export let bookings
+
+onMount(async () => {
+  const res = await fetch(`bookings/list.json`)
+
+  if (res.status === 200) {
+    bookings = await res.json()
+    loading = false
+  } else {
+    console.error(res.status, data.message);
+  }
+})
 </script>
 
 <style>
@@ -41,24 +42,26 @@ export let bookings
 </svelte:head>
 
 <div>
-  {#if !bookings}
-    <h1>No bookings yet</h1>
-  {:else}
-    <h1>Your upcoming bookings</h1>
+  {#if !loading}
+    {#if !bookings}
+      <h1>No bookings yet</h1>
+    {:else}
+      <h1>Your upcoming bookings</h1>
 
-    <div class="bookings">
-      {#each bookings as booking}
-        <div class="booking">
-          <img src="{booking.house.picture}" alt="House picture" />
-          <div>
-            <h2>{booking.house.title} in {booking.house.town}</h2>
-            <p>Booked from {new Date(booking.booking.startDate).toDateString()} to  {new Date(booking.booking.endDate).toDateString()}</p>
-            <p>
-              <a href="/houses/{booking.house.id}">Go to house details</a>
-            </p>
+      <div class="bookings">
+        {#each bookings as booking}
+          <div class="booking">
+            <img src="{booking.house.picture}" alt="House picture" />
+            <div>
+              <h2>{booking.house.title} in {booking.house.town}</h2>
+              <p>Booked from {new Date(booking.booking.startDate).toDateString()} to  {new Date(booking.booking.endDate).toDateString()}</p>
+              <p>
+                <a href="/houses/{booking.house.id}">Go to house details</a>
+              </p>
+            </div>
           </div>
-        </div>
-      {/each}
-    </div>
+        {/each}
+      </div>
+    {/if}
   {/if}
 </div>
--- src/routes/bookings/list.json.js
+++ src/routes/bookings/list.json.js
@@ -1,13 +1,29 @@
 import { Op } from 'sequelize'
 
 import Booking from '../../models/booking.js'
+import User from '../../models/user.js'
 import House from '../../models/house.js'
 
-export function get(req, res, next) {
+export async function get(req, res, next) {
+  if (!req.session.passport || !req.session.passport.user) {
+    res.writeHead(403, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      status: 'error',
+      message: 'Unauthorized'
+    }))
+
+    return
+  }
+
+  const userEmail = req.session.passport.user
+  const user = await User.findOne({ where: { email: userEmail }})
 
   Booking.findAndCountAll({
     where: {
       paid: true,
+      userId: user.id,
       endDate: {
         [Op.gte]: new Date(),
       }

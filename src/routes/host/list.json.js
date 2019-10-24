bf43c73035f718be9cb44da9ad47f897f70516dd
Flavio Copes <copesc@gmail.com>
2019-10-24 16:07:50 +0200

    module-6-lesson-1

---
+++ src/routes/host/list.json.js
@@ -0,0 +1,59 @@
+import { Op } from 'sequelize'
+
+import Booking from '../../models/booking.js'
+import User from '../../models/user.js'
+import House from '../../models/house.js'
+
+export async function get(req, res, next) {
+  if (!req.session.passport || !req.session.passport.user) {
+    res.writeHead(403, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      status: 'error',
+      message: 'Unauthorized'
+    }))
+
+    return
+  }
+
+  const userEmail = req.session.passport.user
+  const user = await User.findOne({ where: { email: userEmail }})
+
+  const houses = await House.findAll({
+    where: {
+      host: user.id
+    }
+  })
+  const houseIds = houses.map(house => house.dataValues.id)
+
+  const bookingsData = await Booking.findAll({
+    where: {
+      paid: true,
+      houseId: {
+        [Op.in]: houseIds
+      },
+      endDate: {
+        [Op.gte]: new Date(),
+      }
+    },
+    order: [
+      ['startDate', 'ASC']
+    ]
+  })
+
+  const bookings = await Promise.all(bookingsData.map(async booking => {
+    return {
+      booking: booking.dataValues,
+      house: houses.filter(house => house.dataValues.id === booking.dataValues.houseId)[0].dataValues
+    }
+  }))
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  })
+  res.end(JSON.stringify({
+    bookings,
+    houses
+  }))
+}

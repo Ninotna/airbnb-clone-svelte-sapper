f9aeaef370409949ee5ebd72affa6542db586aa1
Flavio Copes <copesc@gmail.com>
2019-10-25 19:17:10 +0200

    module-6-lesson-3

---
+++ src/routes/host/_form.svelte
@@ -0,0 +1,126 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+
+  export let house = {}
+  export let buttonText
+
+  let houseTypes = [
+    'Entire house',
+    'Room'
+  ]
+
+	const submit = () => {
+    dispatch('submit', {
+      'house': house
+    })
+  }
+</script>
+
+<style>
+  form p {
+    display: grid;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: 50% 50%;
+  }
+
+  .grid > div {
+    padding: 50px;
+  }
+</style>
+
+<form on:submit|preventDefault={submit}>
+  <p>
+    <label>House title</label>
+    <input required bind:value={house.title} type="text" placeholder="House title" />
+  </p>
+  <p>
+    <label>Town</label>
+    <input required bind:value={house.town} type="text" placeholder="Town" />
+  </p>
+  <p>
+    <label>Price per night</label>
+    <input required bind:value={house.price} type="number" placeholder="Price per night" />
+  </p>
+  <p>
+    <label>House picture URL</label>
+    <input required bind:value={house.picture} type="text" placeholder="House picture URL" />
+  </p>
+  <p>
+    <label>House description</label>
+    <textarea required bind:value={house.description}></textarea>
+  </p>
+
+  <div class="grid">
+    <div>
+      <p>
+        <label>Number of guests</label>
+        <input required bind:value={house.guests} type="number" placeholder="Number of guests" />
+      </p>
+      <p>
+        <label>Number of bedrooms</label>
+        <input required bind:value={house.bedrooms} type="number" placeholder="Number of bedrooms" />
+      </p>
+      <p>
+        <label>Number of beds</label>
+        <input required bind:value={house.beds} type="number" placeholder="Number of beds" />
+      </p>
+      <p>
+        <label>Number of baths</label>
+        <input required bind:value={house.baths} type="number" placeholder="Number of baths" />
+      </p>
+    </div>
+
+    <div>
+      <p>
+        <label>Does it have Wifi?</label>
+        <select bind:value={house.wifi}>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+      </p>
+      <p>
+        <label>Does it have a kitchen?</label>
+        <select bind:value={house.kitchen}>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+      </p>
+      <p>
+        <label>Does it have heating?</label>
+        <select bind:value={house.heating}>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+      </p>
+      <p>
+        <label>Does it have free parking?</label>
+        <select bind:value={house.freeParking}>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+      </p>
+      <p>
+        <label>Is it the entire place?</label>
+        <select bind:value={house.entirePlace}>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+      </p>
+      <p>
+        <label>Type of house</label>
+        <select bind:value={house.type}>
+          {#each houseTypes as item}
+            <option value={item}>
+              {item}
+            </option>
+          {/each}
+        </select>
+      </p>
+    </div>
+  </div>
+  <button class="styled">{buttonText}</button>
+</form>
--- src/routes/host/new.svelte
+++ src/routes/host/new.svelte
@@ -2,16 +2,14 @@
   import axios from 'axios'
   import { goto } from '@sapper/app'
 
-  let houseTypes = [
-    'Entire house',
-    'Room'
-  ]
+import Form from './_form.svelte'
 
 	let house = {}
 
-	const submit = async () => {
+	const submit = async event => {
+    const houseDetails = event.detail.house
     try {
-      const response = await axios.post('host/new', { house })
+      const response = await axios.post('host/new', { 'house': houseDetails })
       if (response.data.status === 'error') {
         if (response.data.message === 'SequelizeValidationError') {
           alert('Validation error')
@@ -50,97 +48,5 @@
 
 <h1>Add a new house</h1>
 <div>
-  <form on:submit|preventDefault={submit}>
-    <p>
-      <label>House title</label>
-      <input required bind:value={house.title} type="text" placeholder="House title" />
-    </p>
-    <p>
-      <label>Town</label>
-      <input required bind:value={house.town} type="text" placeholder="Town" />
-    </p>
-    <p>
-      <label>Price per night</label>
-      <input required bind:value={house.price} type="number" placeholder="Price per night" />
-    </p>
-    <p>
-      <label>House picture URL</label>
-      <input required bind:value={house.picture} type="text" placeholder="House picture URL" />
-    </p>
-    <p>
-      <label>House description</label>
-      <textarea required bind:value={house.description}></textarea>
-    </p>
-
-    <div class="grid">
-      <div>
-        <p>
-          <label>Number of guests</label>
-          <input required bind:value={house.guests} type="number" placeholder="Number of guests" />
-        </p>
-        <p>
-          <label>Number of bedrooms</label>
-          <input required bind:value={house.bedrooms} type="number" placeholder="Number of bedrooms" />
-        </p>
-        <p>
-          <label>Number of beds</label>
-          <input required bind:value={house.beds} type="number" placeholder="Number of beds" />
-        </p>
-        <p>
-          <label>Number of baths</label>
-          <input required bind:value={house.baths} type="number" placeholder="Number of baths" />
-        </p>
-      </div>
-
-      <div>
-        <p>
-          <label>Does it have Wifi?</label>
-          <select bind:value={house.wifi}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </p>
-        <p>
-          <label>Does it have a kitchen?</label>
-          <select bind:value={house.kitchen}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </p>
-        <p>
-          <label>Does it have heating?</label>
-          <select bind:value={house.heating}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </p>
-        <p>
-          <label>Does it have free parking?</label>
-          <select bind:value={house.freeParking}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </p>
-        <p>
-          <label>Is it the entire place?</label>
-          <select bind:value={house.entirePlace}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </p>
-        <p>
-          <label>Type of house</label>
-          <select bind:value={house.type}>
-            {#each houseTypes as item}
-              <option value={item}>
-                {item}
-              </option>
-            {/each}
-          </select>
-        </p>
-      </div>
-    </div>
-
-    <button class="styled">Add house</button>
-  </form>
+  <Form buttonText={'Add house'} on:submit={submit}></Form>
 </div>
